be2b39d8b7a459ace5f94d176ec6851fed668c5b
mahasri <bnzd@qq.com>
2021-05-02 19:58:07 +0800

    添加我的猫

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import cat from './cat.jpeg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+        <p className="title">
+          mahasri cat so cute
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={cat} className="App-logo" alt="logo" />
       </header>
     </div>
   );
